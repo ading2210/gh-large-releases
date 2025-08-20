b65c4fc2963182601a1b9f0bcd6865b1bd470869
ading2210 <ading2019@gmail.com>
2025-08-20 13:39:08 -0700

    fix chunk and manifest generation scripts

--- generate.py
+++ generate.py
@@ -3,9 +3,10 @@ import json
 
 import util
 
-def create_manifest(path, original, original_hash):
+def process_file(original, out_dir):
+  original_hash = util.file_split(original, out_dir, 2*1024*1024*1024)
   original_size = original.stat().st_size
-  chunks = sorted(path.parent.rglob(path.stem + ".*"))
+  chunks = sorted(out_dir.parent.rglob(f"{original.name}.*"))
   chunk_size = chunks[0].stat().st_size
   chunk_names = [c.name for c in chunks if c.suffix != ".manifest"]
   
@@ -17,16 +18,13 @@ def create_manifest(path, original, original_hash):
     "chunks": chunk_names
   }
   manifest_json = json.dumps(manifest, indent=2)
-  path.write_text(manifest_json)
-
-def process_file(path):
-  original_hash = util.file_split(original_zip, 2*1024*1024*1024)
-  manifest_path = path.with_name(path.name + ".manifest")
-  create_manifest(manifest_path, original_zip, original_hash)
+  manifest_path = out_dir / f"{original.name}.manifest"
+  manifest_path.write_text(manifest_json)
 
 if __name__ == "__main__":
   base_dir = pathlib.Path(__file__).resolve().parent
   original_dir = base_dir / "data"
+  out_dir = base_dir / "out"
 
   for original_zip in original_dir.rglob("*.zip"):
-    process_file(original_zip)
+    process_file(original_zip, out_dir)
