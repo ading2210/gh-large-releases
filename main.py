fde2486960bff25db0873c4b287372178b5ac8e3
ading2210 <ading2019@gmail.com>
2025-09-18 13:52:14 -0700

    many bugfixes

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ import pathlib
 import hashlib
 import json
 import math
-import threading
-import queue
 import re
 import logging
 
 import httpx
-from urllib3.util.retry import Retry
-from requests.adapters import HTTPAdapter
 
 session = httpx.Client(follow_redirects=True)
 
@@ -19,37 +15,41 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def get_size(total_size, chunk_size, chunk_count, i):
-  if i == chunk_count - 1:
+  if i == chunk_count - 1 and total_size % chunk_size > 0:
     return total_size % chunk_size
   return chunk_size
 
-def human_readable_size(size, decimal_places=2):
+def pretty_size(size, decimal_places=2):
   for unit in ["B", "KiB", "MiB", "GiB", "TiB", "PiB"]:
     if size < 1024.0 or unit == "PiB":
       break
     size /= 1024.0
   return f"{size:.{decimal_places}f} {unit}"
 
-def upload_asset(args, name, data, length):
-  release = get_release(args)
+def upload_asset(args, release, name, data, length):
+  for asset in release["assets"]:
+    if asset["name"] == name:
+      raise RuntimeError(f"asset {name} has already been uploaded")
+
   url = f"https://uploads.github.com/repos/{args.repository}/releases/{release['id']}/assets?name={name}"
-  r = session.post(url, data=data, headers={
+  r = session.post(url, data=data, timeout=httpx.Timeout(None), headers={
     "Content-Type": "application/octet-stream",
     "Content-Length": str(length)
   })
   r.raise_for_status()
 
-def process_file(args, path):
+def process_file(args, release, path):
   chunk_names = []
   original_size = path.stat().st_size
-  #big_chunk_size = min(original_size, 2*1024*1024*1024)
-  big_chunk_size = min(original_size, 40*1024*1024)
+  big_chunk_size = int(args.big_chunk_size) if args.big_chunk_size else 2*1024*1024*1024
+  big_chunk_size = min(original_size, big_chunk_size)
   small_chunk_size = 25*1024*1024 
 
   total_size = path.stat().st_size
   big_chunks = math.ceil(total_size / big_chunk_size)
-  sha256sum = hashlib.sha256()
+  sha_hash = hashlib.sha256()
 
+  total_uploaded = 0
   with open(path, "rb") as read_file:
     for i in range(0, big_chunks):
       new_name = f"{path.name}.{i:04}"
@@ -57,19 +57,21 @@ def process_file(args, path):
       small_chunks = math.ceil(big_size / small_chunk_size)
 
       def chunk_generator():
+        nonlocal total_uploaded
         for j in range(0, small_chunks):
           small_size = get_size(big_size, small_chunk_size, small_chunks, j)
           data = read_file.read(small_size)
-          sha256sum.update(data)
-          print(len(data))
+          sha_hash.update(data)
           yield data
+          total_uploaded += len(data)
+          logger.info(f"uploaded {pretty_size(total_uploaded)} / {pretty_size(total_size)} of {path.name}")
       
       chunk_names.append(new_name)
-      upload_asset(args, new_name, chunk_generator(), big_size)
+      upload_asset(args, release, new_name, chunk_generator(), big_size)
 
   manifest = {
     "name": path.name,
-    "hash": sha256sum.hexdigest(),
+    "hash": sha_hash.hexdigest(),
     "size": original_size,
     "chunk_size": big_chunk_size,
     "chunks": chunk_names
@@ -77,18 +79,20 @@ def process_file(args, path):
   manifest_json = json.dumps(manifest, indent=2).encode()
   manifest_name = f"{path.name}.manifest"
 
-  upload_asset(args, manifest_name, manifest_json, len(manifest_json))
+  upload_asset(args, release, manifest_name, manifest_json, len(manifest_json))
   update_release_body(args)
 
 #get the release we will use, creating one if needed
-def get_release(args):
-  url = f"https://api.github.com/repos/{args.repository}/releases/tags/{args.tag_name}"
+def get_release(args, retry=False):
+  url = f"https://api.github.com/repos/{args.repository}/releases"
   r = session.get(url)
+  r.raise_for_status()
+  releases = r.json()
 
-  if r.status_code == 200:
-    return r.json()
-  else:
-    return create_release(args)
+  for release in releases:
+    if release["tag_name"] == args.tag_name:
+      return release
+  return create_release(args)
 
 #create a new release
 def create_release(args):
@@ -120,30 +124,42 @@ def update_release_body(args):
   ]
   release = get_release(args)
   
+  manifests = []
   for asset in release["assets"]:
     if not asset["name"].endswith(".manifest"):
       continue
     r = session.get(asset["url"], headers={
       "Accept": "application/octet-stream"
     })
-    print(r.content)
-    manifest = r.json()
-    print(manifest)
-    table_lines.append(f"| {manifest['name']} | {human_readable_size(manifest['size'])} | `{manifest['hash']}` |")
+    manifests.append(r.json())
+
+  manifests.sort(key=lambda x: x["name"])
+  for manifest in manifests:
+    table_lines.append(f"| {manifest['name']} | {pretty_size(manifest['size'])} | `{manifest['hash']}` |")
+
   table_lines.append(tag_end)
   table_str = "\n".join(table_lines)
-  
   table_regex = f"{tag_start}.+{tag_end}"
   body = release["body"] or ""
-  if re.findall(table_regex, body):
+
+  if re.findall(table_regex, body, flags=re.S):
     body = re.sub(table_regex, table_str, body, flags=re.S)
   else:
     body += f"\n\n{table_str}"
   
   url = f"https://api.github.com/repos/{args.repository}/releases/{release['id']}"
-  r = session.patch(url, json={
-    "body": body
-  })
+  payload = {
+    "tag_name": args.tag_name,
+    "target_commitish": args.target_commitish or None,
+    "name": args.name or None, 
+    "body": body,
+    "draft": json.loads(args.draft) if args.draft else None,
+    "prerelease": json.loads(args.prerelease) if args.prerelease else None,
+    "discussion_category_name": args.discussion_category_name or None,
+    "make_latest": args.make_latest or None
+  }
+  payload = {k: v for k, v in payload.items() if v is not None}
+  r = session.patch(url, json=payload)
   r.raise_for_status()
 
 if __name__ == "__main__":
@@ -161,6 +177,7 @@ if __name__ == "__main__":
   parser.add_argument("--make_latest")
   parser.add_argument("--generate_release_notes")
   parser.add_argument("--discussion_category_name")
+  parser.add_argument("--big_chunk_size")
   args = parser.parse_args()
 
   session.headers.update({
@@ -171,6 +188,6 @@ if __name__ == "__main__":
   release = get_release(args)
 
   base_path = pathlib.Path(args.workspace).resolve()
-  for file_name in args.files.split("\n"):
-    file_path = base_path / file_name
-    process_file(args, file_path)
+  for file_glob in args.files.split("\n"):
+    for file_path in base_path.glob(file_glob.strip()):
+      process_file(args, release, file_path)
